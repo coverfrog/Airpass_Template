a3cbde18958c53ee6c2102c7a8f8de3dafb182a1
ChangSik Kim <justfroglab@gmail.com>
2024-08-12 17:22:23 +0900

    TemplateUpdate

--- Assets/Core/1_Scripts/Anim/AnimScale.cs
+++ Assets/Core/1_Scripts/Anim/AnimScale.cs
@@ -1,11 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Sirenix.OdinInspector;
 
 namespace CoverFrog
 {
     public class AnimScale : Anim
     {
+        [Title("AnimScale")] 
+        [SerializeField] private float delay;
+        [SerializeField] private float duration;
+        [Space]
+        [SerializeField] private float startScale = 1.0f;
+        [SerializeField] private float endScale = 1.0f;
+
+        public override void OnEnable()
+        {
+            base.OnEnable();
+
+            transform.localScale = Vector3.one * startScale;
+        }
+
         public override void Init(params object[] values)
         {
             
@@ -13,7 +28,15 @@ namespace CoverFrog
 
         protected override IEnumerator CoPlay(params object[] values)
         {
-            yield return null;
+            yield return new WaitForSeconds(delay);
+
+            for (var t = 0.0f; t < duration; t += Time.deltaTime)
+            {
+                transform.localScale = Vector3.one * Mathf.Lerp(startScale, endScale, t / duration);
+                yield return null;
+            }
+
+            transform.localScale = Vector3.one * endScale;
         }
     }
 }
